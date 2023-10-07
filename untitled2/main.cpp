d11f2c6c964a59c1c3e11dc63df107fcaebde425
Valeria-XX <rakushk46@mail.ru>
2023-10-07 15:49:18 +0700

    write a program skeleton for the second lab and several functions

---
+++ untitled2/main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char* argv[]) {
+
+    QApplication
+        QApplication app(argc, argv);
+    MainWindow mainWindow;
+    mainWindow.
+        MainWindow mainWindow;
+
+
+    MainWindow
+        show();
+    return app.exec();
+
+}
